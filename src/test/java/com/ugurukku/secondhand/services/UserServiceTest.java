4facc1bd4a9a86f00cbfacb5c183224b6f9c0a66
Ugur <ugurkarimli1@gmail.com>
2022-12-18 04:37:23 +0400

    added getByEmail testing

--- src/test/java/com/ugurukku/secondhand/services/UserServiceTest.java
+++ src/test/java/com/ugurukku/secondhand/services/UserServiceTest.java
@@ -6,16 +6,19 @@ import com.ugurukku.secondhand.dto.UserDto;
 import com.ugurukku.secondhand.dto.UserDtoConverter;
 import com.ugurukku.secondhand.models.UserInformation;
 import com.ugurukku.secondhand.repositories.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest extends TestSupport {
@@ -31,21 +34,37 @@ public class UserServiceTest extends TestSupport {
 
     @Test
     public void testGetAllUsers_itShouldReturnUserDtoList() {
-
-
         List<UserInformation> userList = generateUsers();
         List<UserDto> userDtoList = generateUserDtoList(userList);
 
-        Mockito.when(repository.findAll()).thenReturn(userList);
-        Mockito.when(converter.convert(userList)).thenReturn(generateUserDtoList(userList));
-
+        when(repository.findAll()).thenReturn(userList);
+        when(converter.convert(userList)).thenReturn(generateUserDtoList(userList));
 
 
         List<UserDto> result = service.getAll();
 
-        Assertions.assertEquals(result,userDtoList);
-        Mockito.verify(repository).findAll();
-        Mockito.verify(converter).convert(userList);
+        assertEquals(result, userDtoList);
+        verify(repository).findAll();
+        verify(converter).convert(userList);
+    }
+
+    @Test
+    public void testGetByEmail_whenUserMailExists_itShouldReturnUserDto() {
+
+        UserInformation userInformation = generateUserInformation();
+        UserDto userDto = generateUserDto(userInformation);
+
+        when(repository.findUserByEmail("ugur@com")).thenReturn(Optional.of(userInformation));
+        when(converter.convert(userInformation)).thenReturn(userDto);
+
+        UserDto result = service.getByEmail("ugur@com");
+
+        assertEquals(result, userDto);
+
+
+        verify(repository).findUserByEmail("ugur@com");
+        verify(converter).convert(userInformation);
+
     }
 
 }
