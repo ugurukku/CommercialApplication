30553cc0f7f816f3fe07edef34aa644f3511742b
Ugur <ugurkarimli1@gmail.com>
2023-01-11 22:06:09 +0400

    some changes

--- src/main/java/com/ugurukku/secondhand/dto/AdvertisementDtoConverter.java
+++ src/main/java/com/ugurukku/secondhand/dto/AdvertisementDtoConverter.java
@@ -3,8 +3,10 @@ package com.ugurukku.secondhand.dto;
 import com.ugurukku.secondhand.models.Advertisement;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component
 public interface AdvertisementDtoConverter {
 
     AdvertisementDtoConverter INSTANCE = Mappers.getMapper(AdvertisementDtoConverter.class);
--- src/main/java/com/ugurukku/secondhand/services/AdvertisementService.java
+++ src/main/java/com/ugurukku/secondhand/services/AdvertisementService.java
@@ -1,7 +1,9 @@
 package com.ugurukku.secondhand.services;
 
 import com.ugurukku.secondhand.dto.AdvertisementDto;
+import com.ugurukku.secondhand.dto.AdvertisementDtoConverter;
 import com.ugurukku.secondhand.dto.CreateAdvertisementRequest;
+import com.ugurukku.secondhand.models.Advertisement;
 import com.ugurukku.secondhand.repositories.AdvertisementRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +12,24 @@ public class AdvertisementService {
 
     private final AdvertisementRepository repository;
 
-    public AdvertisementService(AdvertisementRepository repository) {
+    private final AdvertisementDtoConverter converter;
+
+    public AdvertisementService(AdvertisementRepository repository, AdvertisementDtoConverter converter) {
         this.repository = repository;
+        this.converter = converter;
     }
 
-//    public AdvertisementDto createAdvertisement(CreateAdvertisementRequest request){
+    public AdvertisementDto createAdvertisement(CreateAdvertisementRequest request) {
 
-//    }
+        return converter
+                .convertDto(
+                        repository.save(new Advertisement(
+                                request.title(),
+                                request.description(),
+                                request.price(),
+                                request.userId(),
+                                request.hashtags())));
+    }
 
 
 }
